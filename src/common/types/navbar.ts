86d1a7e258ad37a9545091d18c6918345c47da82
Swanand-Wagh <swanu@uw.edu>
2024-07-25 02:34:34 -0700

    navabr constants & types added

---
+++ src/common/types/navbar.ts
@@ -0,0 +1,12 @@
+export interface NavbarOption {
+  name: string;
+  url: string;
+  description: string;
+}
+
+export interface NavbarProps {
+  name: string;
+  url?: string;
+  type: 'Link' | 'Dropdown' | 'Button';
+  options?: NavbarOption[];
+}
