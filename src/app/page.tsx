56f236fa0f276521afbf6fa1a48162b638e78add
Swanand-Wagh <swanu@uw.edu>
2024-07-25 02:22:49 -0700

    added Navbar folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import Home from '@/modules/home';
+
 const HomePage = () => {
-  return <>HomePage</>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 };
 
 export default HomePage;
