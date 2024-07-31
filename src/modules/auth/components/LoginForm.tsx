d154eee213a4f8cdf7d034dfe7b8fd47fee10b8e
Swanand-Wagh <swanu@uw.edu>
2024-07-31 14:40:36 -0700

    added schema validations to auth

--- src/modules/auth/components/LoginForm.tsx
+++ src/modules/auth/components/LoginForm.tsx
@@ -3,34 +3,79 @@
 import React from 'react';
 import Link from 'next/link';
 
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
 import { Button } from '@/common/components/ui/button';
 import { Input } from '@/common/components/ui/input';
-import { Label } from '@/common/components/ui/label';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/common/components/ui/form';
 
-export const LoginForm = () => {
+const loginSchema = z.object({
+  email: z.string().email({
+    message: 'Invalid email address.',
+  }),
+  password: z.string().min(1, {
+    message: 'Password is required.',
+  }),
+});
+
+export function LoginForm() {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof loginSchema>) => {
+    console.log(data);
+  };
+
   return (
-    <div className="grid gap-4">
-      <div className="grid gap-2">
-        <Label htmlFor="email">Email</Label>
-        <Input id="email" type="email" placeholder="m@example.com" required />
-      </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4">
+        <FormField
+          name="email"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="m@example.com" {...field} className={fieldState.invalid ? 'border-red-500' : ''} />
+              </FormControl>
+              <FormMessage>{fieldState.error?.message}</FormMessage>
+            </FormItem>
+          )}
+        />
 
-      <div className="grid gap-2">
-        <div className="flex items-center">
-          <Label htmlFor="password">Password</Label>
-          <Link href="/forgot-password" className="ml-auto inline-block text-sm underline">
-            Forgot your password?
-          </Link>
-        </div>
-        <Input id="password" type="password" required />
-      </div>
+        <FormField
+          name="password"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <FormItem>
+              <div className="flex items-center">
+                <FormLabel>Password</FormLabel>
+                <Link href="/forgot-password" className="ml-auto inline-block text-sm underline">
+                  Forgot your password?
+                </Link>
+              </div>
+              <FormControl>
+                <Input type="password" {...field} className={fieldState.invalid ? 'border-red-500' : ''} />
+              </FormControl>
+              <FormMessage>{fieldState.error?.message}</FormMessage>
+            </FormItem>
+          )}
+        />
 
-      <Button type="submit" className="w-full">
-        Login
-      </Button>
-      <Button variant="outline" className="w-full">
-        Login with Google
-      </Button>
-    </div>
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+        <Button variant="outline" className="w-full">
+          Login with Google
+        </Button>
+      </form>
+    </Form>
   );
-};
+}
--- src/modules/auth/components/RegisterForm.tsx
+++ src/modules/auth/components/RegisterForm.tsx
@@ -1,39 +1,117 @@
 'use client';
 
 import React from 'react';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 import { Button } from '@/common/components/ui/button';
 import { Input } from '@/common/components/ui/input';
-import { Label } from '@/common/components/ui/label';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/common/components/ui/form';
 
-export const RegisterForm = () => {
+const registerSchema = z.object({
+  firstName: z.string().min(1, {
+    message: 'First name is required.',
+  }),
+  lastName: z.string().min(1, {
+    message: 'Last name is required.',
+  }),
+  email: z.string().email({
+    message: 'Invalid email address.',
+  }),
+  password: z.string().min(1, {
+    message: 'Password is required.',
+  }),
+});
+
+export function RegisterForm() {
+  const form = useForm<z.infer<typeof registerSchema>>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof registerSchema>) => {
+    console.log(data);
+  };
+
   return (
-    <div className="grid gap-4">
-      <div className="grid grid-cols-2 gap-4">
-        <div className="grid gap-2">
-          <Label htmlFor="first-name">First name</Label>
-          <Input id="first-name" placeholder="Max" required />
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="last-name">Last name</Label>
-          <Input id="last-name" placeholder="Robinson" required />
-        </div>
-      </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4">
+        <div className="grid grid-cols-2 gap-4">
+          <FormField
+            name="firstName"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <FormItem>
+                <FormLabel>First name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Max" {...field} className={fieldState.invalid ? 'border-red-500' : ''} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-      <div className="grid gap-2">
-        <Label htmlFor="email">Email</Label>
-        <Input id="email" type="email" placeholder="m@example.com" required />
-      </div>
-      <div className="grid gap-2">
-        <Label htmlFor="password">Password</Label>
-        <Input id="password" type="password" />
-      </div>
-      <Button type="submit" className="w-full">
-        Create an account
-      </Button>
-      <Button variant="outline" className="w-full">
-        Sign up with Google
-      </Button>
-    </div>
+          <FormField
+            name="lastName"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <FormItem>
+                <FormLabel>Last name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Robinson" {...field} className={fieldState.invalid ? 'border-red-500' : ''} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <FormField
+          name="email"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input
+                  type="email"
+                  placeholder="m@example.com"
+                  {...field}
+                  className={fieldState.invalid ? 'border-red-500' : ''}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          name="password"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" {...field} className={fieldState.invalid ? 'border-red-500' : ''} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" className="w-full">
+          Create an account
+        </Button>
+        <Button variant="outline" className="w-full">
+          Sign up with Google
+        </Button>
+      </form>
+    </Form>
   );
-};
+}
