c47ad198ae295b882fc170f629a4dc5a37ab8573
Swanand-Wagh <swanu@uw.edu>
2024-10-19 03:11:23 -0700

    misc

--- src/modules/admin/index.ts
+++ src/modules/admin/index.ts
@@ -1,5 +1,4 @@
-import { Sidebar } from './components/Sidebar';
 import { columns } from './components/columns';
 import { UserDataTable } from './components/table';
 
-export { Sidebar, columns, UserDataTable };
+export { columns, UserDataTable };
