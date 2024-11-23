092741c1ac0ac74bd748976bf7405c046a674369
Swanand-Wagh <swanandwagh7@gmail.com>
2024-11-22 19:52:41 -0800

    forced / & /about pages to be statically rendered

--- src/app/(public)/about/page.tsx
+++ src/app/(public)/about/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+export const dynamic = 'force-static';
 const AboutPage = () => {
   return <>AboutPage</>;
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Home from '@/modules/home';
 import { Navbar, Footer } from '@/common/components/custom';
 
+export const dynamic = 'force-static';
 const HomePage = () => {
   return (
     <>
