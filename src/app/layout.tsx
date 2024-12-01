60ed354ff546ff24f7325398fd66104e7af9680c
Swanand-Wagh <swanandwagh7@gmail.com>
2024-12-01 02:49:54 -0800

    rolledback SessionProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,15 @@
 import type { Metadata } from 'next';
 
 import { auth } from '@/auth';
-import { SessionProvider } from '@/common/components/providers/SessionProvider';
+import { SessionProvider } from 'next-auth/react';
 
 import { Toaster } from '@/common/components/ui/toaster';
 import { ToastProvider } from '@/common/components/ui/toast';
 
 import { Plus_Jakarta_Sans, Inter } from 'next/font/google';
 import '@/common/styles/globals.css';
-import { Session } from 'next-auth';
 
-const plus_jakarta_sans = Plus_Jakarta_Sans({
-  subsets: ['latin'],
-  preload: true,
-});
+const plus_jakarta_sans = Plus_Jakarta_Sans({ subsets: ['latin'], preload: true });
 const inter = Inter({ subsets: ['latin'], preload: true });
 
 export const metadata: Metadata = {
@@ -23,13 +19,13 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
   children,
-  session,
 }: Readonly<{
   children: React.ReactNode;
-  session: Session;
 }>) {
+  const session = await auth();
+
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={session} refetchInterval={0} refetchOnWindowFocus={false}>
       <html lang="en">
         <ToastProvider>
           <body className={`${plus_jakarta_sans.className} ${inter.className}`} suppressHydrationWarning={true}>
