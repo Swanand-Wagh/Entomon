ed2842751c1672cc6d73aa37ac3e937e220ed527
Swanand-Wagh <swanandwagh7@gmail.com>
2025-05-06 21:02:06 -0700

    misc

--- src/features/blog/components/BlogListLayout.tsx
+++ src/features/blog/components/BlogListLayout.tsx
@@ -1,12 +1,11 @@
 'use client';
 
-import React, { useEffect, useMemo } from 'react';
-// import { BlogsWithoutContent } from '../server/repo';
+import React, { useMemo } from 'react';
 import { categoryFilterList, priceFilterList } from '@/constants/filterLists';
 import { BlogDataType, CategoriesFilterList, PriceFilterList } from '../types/blog';
 import { FilterList } from './FilterList';
 import { BlogList } from './BlogList';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import { RenderPagination } from '@/components/custom/RenderPagination';
 
 type BlogListLayoutProps = {
@@ -18,12 +17,9 @@ export const PRICE_QUERY_KEY = 'price';
 export const ITEMS_PER_PAGE = 12;
 
 export const BlogListLayout: React.FC<Readonly<BlogListLayoutProps>> = ({ data }) => {
-  const router = useRouter();
-  const pathname = usePathname();
   const searchParams = useSearchParams();
-  const currentCategory = searchParams.get(CATEGORY_QUERY_KEY);
-  const currentPrice = searchParams.get(PRICE_QUERY_KEY);
-  const currentPage = searchParams.get('page');
+  const currentCategory = searchParams.get(CATEGORY_QUERY_KEY) ?? "all";
+  const currentPrice = searchParams.get(PRICE_QUERY_KEY) ?? "all";
 
   const filteredBlogs = useMemo(() => {
     if (currentCategory === 'all' && currentPrice === 'all') return data;
@@ -32,53 +28,6 @@ export const BlogListLayout: React.FC<Readonly<BlogListLayoutProps>> = ({ data }
 
     return data.filter((d) => d.categories.includes(currentCategory ?? '') && d.isPaid === (currentPrice === 'paid'));
   }, [data, currentCategory, currentPrice]);
-
-  useEffect(() => {
-    let query: URLSearchParams;
-
-    if (searchParams.size === 0) query = new URLSearchParams();
-    else query = new URLSearchParams(searchParams.toString());
-
-    let queryChanged = false;
-
-    if (!currentCategory) {
-      query.set(CATEGORY_QUERY_KEY, 'all');
-      queryChanged = true;
-    } else {
-      const categoryExists = categoryFilterList.some((category) => category.value === currentCategory);
-
-      if (!categoryExists) {
-        query.set(CATEGORY_QUERY_KEY, 'all');
-        queryChanged = true;
-      }
-    }
-
-    if (!currentPrice) {
-      query.set(PRICE_QUERY_KEY, 'all');
-      queryChanged = true;
-    } else {
-      const priceExists = priceFilterList.some((price) => price.value === currentPrice);
-
-      if (!priceExists) {
-        query.set(PRICE_QUERY_KEY, 'all');
-        queryChanged = true;
-      }
-    }
-
-    if (!currentPage) {
-      query.set('page', '1');
-      queryChanged = true;
-    } else {
-      const page = parseInt(currentPage);
-
-      if (Number.isNaN(page) || page < 1) {
-        query.set('page', '1');
-        queryChanged = true;
-      }
-    }
-
-    if (queryChanged) router.replace(`${pathname}?${query.toString()}`, { scroll: false });
-  }, [pathname, router]);
 
   return (
     <React.Fragment>
--- src/features/events/components/EventListLayout.tsx
+++ src/features/events/components/EventListLayout.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import React, { useEffect, useMemo } from 'react';
-
+import React, { useMemo } from 'react';
 import { EventWithoutDescriptionType } from '../types/event';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import { categoryFilterList, priceFilterList } from '@/constants/filterLists';
 import { FilterList } from '@/features/blog/components/FilterList';
 import { CategoriesFilterList, PriceFilterList } from '@/features/blog/types/blog';
@@ -19,12 +18,9 @@ export const PRICE_QUERY_KEY = 'price';
 export const ITEMS_PER_PAGE = 12;
 
 export const EventListLayout = ({ events }: EventListProps) => {
-  const router = useRouter();
-  const pathname = usePathname();
   const searchParams = useSearchParams();
-  const currentCategory = searchParams.get(CATEGORY_QUERY_KEY);
-  const currentPrice = searchParams.get(PRICE_QUERY_KEY);
-  const currentPage = searchParams.get('page');
+  const currentCategory = searchParams.get(CATEGORY_QUERY_KEY) ?? "all";
+  const currentPrice = searchParams.get(PRICE_QUERY_KEY) ?? "all";
 
   const filteredEvents = useMemo(() => {
     if (!currentCategory || !currentPrice) return events;
@@ -39,53 +35,6 @@ export const EventListLayout = ({ events }: EventListProps) => {
       return d.categories.includes(currentCategory) && isPaid === (currentPrice === 'paid');
     });
   }, [events, currentCategory, currentPrice]);
-
-  useEffect(() => {
-    let query: URLSearchParams;
-
-    if (searchParams.size === 0) query = new URLSearchParams();
-    else query = new URLSearchParams(searchParams.toString());
-
-    let queryChanged = false;
-
-    if (!currentCategory) {
-      query.set(CATEGORY_QUERY_KEY, 'all');
-      queryChanged = true;
-    } else {
-      const categoryExists = categoryFilterList.some((category) => category.value === currentCategory);
-
-      if (!categoryExists) {
-        query.set(CATEGORY_QUERY_KEY, 'all');
-        queryChanged = true;
-      }
-    }
-
-    if (!currentPrice) {
-      query.set(PRICE_QUERY_KEY, 'all');
-      queryChanged = true;
-    } else {
-      const priceExists = priceFilterList.some((price) => price.value === currentPrice);
-
-      if (!priceExists) {
-        query.set(PRICE_QUERY_KEY, 'all');
-        queryChanged = true;
-      }
-    }
-
-    if (!currentPage) {
-      query.set('page', '1');
-      queryChanged = true;
-    } else {
-      const page = parseInt(currentPage);
-
-      if (Number.isNaN(page) || page < 1) {
-        query.set('page', '1');
-        queryChanged = true;
-      }
-    }
-
-    if (queryChanged) router.replace(`${pathname}?${query.toString()}`, { scroll: false });
-  }, [pathname, router]);
 
   return (
     <React.Fragment>
